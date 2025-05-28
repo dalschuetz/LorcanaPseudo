fe68ff6450f75141593973d27c46c9643e4ac23a
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-05-28 11:25:15 -0600

    input manager added, reorganized battle manager

---
+++ Scripts/Managers/InputManager.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+#when mouse goes over something, enlarge it?
+
+#allow for clicking buttons and cards and stuff, raycasting
