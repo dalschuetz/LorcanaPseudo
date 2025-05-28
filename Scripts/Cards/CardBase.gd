810fee7df7566e80c4a42bf85b78c34bbd4577e7
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-05-28 11:36:28 -0600

    PlayerCard, OpponentCard, BaseCard

---
+++ Scripts/Cards/CardBase.gd
@@ -0,0 +1,11 @@
+extends Node
+
+#lots of information here
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+#initialize from data function
---
+++ Scripts/Cards/PlayerCard.gd
@@ -0,0 +1,32 @@
+extends Node
+
+#discard = 0
+#hand = 0
+#inkwell = 0
+#playzone = 0
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+	
+#set if hovered over on
+
+#set if hovered over off
+
+#set is in discard function
+
+#remove is in discard function
+
+#set is in hand function
+
+#remove is in hand function
+
+#set is in inkwell function
+
+#remove is in inkwell function
+
+#set is in playzone function
+
+#remove is in playzone function
