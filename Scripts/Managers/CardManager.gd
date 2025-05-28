b41f858b53a90ad60f66b94d53a89d8c2af58f0a
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-05-28 11:29:03 -0600

    added card manager: card manager and input manager need work

---
+++ Scripts/Managers/CardManager.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+#hover over card to make it bigger
+	#maybe on hover, the buttons for actions (quest, attack, or ink) appear?
+
+#raycasting stuff
+
+#allow for dragging cards? or will we just have buttons appear on the cards? idk
